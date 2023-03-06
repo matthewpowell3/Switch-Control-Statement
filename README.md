6d81dba34e53dea90c6cd3df73692273e83da130
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-05 21:44:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Switch-Control-Statement
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-mflug3)
---
+++ script.js
@@ -0,0 +1,9 @@
+// Switch Control Statement
+
+// 7 -> seven
+
+var month = 1;
+switch (month) {
+  case 1:
+    document.write('January');
+}
