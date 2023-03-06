b614a43a2099bd7b008a68301d9b6b5e6024f3c4
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-05 21:58:37 -0500

    1 file modified

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // 7 -> seven
 
-var month = 1;
+var month = 5;
 switch (month) {
   case 1:
     document.write('January');
@@ -10,6 +10,44 @@ switch (month) {
 
   case 2:
     document.write('February');
+    break;
+
+  case 3:
+    document.write('March');
+    break;
+
+  case 4:
+    document.write('April');
+    break;
+
+  case 5:
+    document.write('May');
+    break;
+
+  case 6:
+    document.write('June');
+    break;
+
+  case 7:
+    document.write('July');
+    break;
+
+  case 8:
+    document.write('August');
+    break;
+
+  case 9:
+    document.write('September');
+    break;
+  case 10:
+    document.write('October');
+    break;
+  case 11:
+    document.write('November');
+    break;
+  case 12:
+    document.write('December');
+    break;
 }
 
 // if you don't break it case 1 and case 2 will display both
