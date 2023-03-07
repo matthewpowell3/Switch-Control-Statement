3743d0d06c30ea61bdb9398cf8980512f2256cd3
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-06 19:02:26 -0500

    1 file modified

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // 7 -> seven
 
 /*
-var month = -5;
+var month = 0;
 switch (month) {
   case 1:
     document.write('January');
@@ -52,10 +52,25 @@ switch (month) {
   case 12:
     document.write('December');
     break;
-} */
+
+  default:
+    document.write('No month with this number');
+}
+*/
 
 // default will happen if the number is greater than 12 or less than or equal to zero -5
 // if you don't break it case 1 and case 2 will display both
 
+var month = 'June';
+switch (month) {
+  case 'June':
+    document.write('sixth month');
+    break;
 
-var 
+  case 'July':
+    document.write('seventh month');
+    break;
+
+  default:
+    document.write('No month with this number');
+}
